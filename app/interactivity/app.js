9ba2cc66d508f29e1187474a5bde2b1f796c1cc9
Frag Spawn <frag.spawn@gmail.com>
2021-08-08 17:48:28 +1000

    add html & fetch requests

--- app/interactivity/app.js
+++ app/interactivity/app.js
@@ -1,1 +1,77 @@
 console.log('todo app');
+
+function login (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function register (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function userexists (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function userloggedin (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function getcategories (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function addcategory (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function delcategory (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function updatecategory (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function gettodos (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function gettodos (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+
+function addtodo (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+function deltodo (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
+function updatetodo (evt) {
+    evt.preventDefault ();
+    method = {method: evt.target.method};
+    fetch(evt.target.action, method);
+}
--- server.js
+++ server.js
@@ -3,7 +3,46 @@ const app = express()
 
 app.use('/', express.static('app'))
 
-app.get('/api', function (req, res) {
+// authentication
+app.post('/api/login', function (req, res) {
+    res.send('Test login')
+})
+app.put('/api/register', function (req, res) {
+    res.send('Test register')
+})
+app.get('/api/isloggedin', function (req, res) {
+    res.send('Test isLoggedin')
+})
+app.get('/api/doesuserexist', function (req, res) {
+    res.send('Test doesuserexist')
+})
+
+
+// todo categories CRUD
+app.get('/api/getcat', function (req, res) {
+    res.send('Test Get categories')
+})
+app.put('/api/addcat', function (req, res) {
+    res.send('Test add categories')
+})
+app.delete('/api/delcat/:catid', function (req, res) {
+    res.send('Test del categories')
+})
+app.patch('/api/updatecat/:catid', function (req, res) {
+    res.send('Test update categories')
+})
+
+// todo list CRUD
+app.get('/api/gettodos/:catid', function (req, res) {
+    res.send('Test the API')
+})
+app.put('/api/addtodo', function (req, res) {
+    res.send('Test the API')
+})
+app.delete('/api/deltodo/:todoid', function (req, res) {
+    res.send('Test the API')
+})
+app.patch('/api/updatetodo/:todoid', function (req, res) {
     res.send('Test the API')
 })
 
