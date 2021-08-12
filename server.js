b01f7a6dd6478f8a3034fd8a803c5aa165a7f657
Frag Spawn <frag.spawn@gmail.com>
2021-08-12 15:17:48 +1000

    add body-parser module to scan body

--- server.js
+++ server.js
@@ -1,50 +1,72 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+const jsonParser = bodyParser.json()
 
 app.use('/', express.static('app'))
 
 // authentication
-app.post('/api/login', function (req, res) {
+app.post('/api/login', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(200)
     res.send('Test login')
 })
-app.put('/api/register', function (req, res) {
+app.put('/api/register', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(201)
     res.send('Test register')
 })
 app.get('/api/isloggedin', function (req, res) {
+    res.status(200)
     res.send('Test isLoggedin')
 })
 app.get('/api/doesuserexist', function (req, res) {
+    res.status(200)
     res.send('Test doesuserexist')
 })
 app.get('/api/logout', function (req, res) {
+    res.status(200)
     res.send('Test logout')
 })
 
 // todo categories CRUD
 app.get('/api/getcat', function (req, res) {
+    res.status(200)
     res.send('Test Get categories')
 })
-app.put('/api/addcat', function (req, res) {
+app.put('/api/addcat', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(201)
     res.send('Test add categories')
 })
 app.delete('/api/delcat/:catid', function (req, res) {
+    res.status(202)
     res.send('Test del categories')
 })
-app.patch('/api/updatecat/:catid', function (req, res) {
+app.patch('/api/updatecat/:catid', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(202)
     res.send('Test update categories')
 })
 
 // todo list CRUD
 app.get('/api/gettodos/:catid', function (req, res) {
+    res.status(200)
     res.send('Test get todos')
 })
-app.put('/api/addtodo', function (req, res) {
+app.put('/api/addtodo', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(201)
     res.send('Test add todo')
 })
 app.delete('/api/deltodo/:todoid', function (req, res) {
+    res.status(202)
     res.send('Test del todo')
 })
-app.patch('/api/updatetodo/:todoid', function (req, res) {
+app.patch('/api/updatetodo/:todoid', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.status(202)
     res.send('Test update todo')
 })
 
